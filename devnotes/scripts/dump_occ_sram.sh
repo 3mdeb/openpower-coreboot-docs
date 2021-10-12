b23078cc4b0aabddc6c6426df79d9e92dbf5dca1
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-10-12 12:37:51 +0200

    devnotes/scripts/awk_program_occ: work on data split into 4B chunks
    This minimises amount of manual work required when trace buffer overflows.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- devnotes/scripts/dump_occ_sram.sh
+++ devnotes/scripts/dump_occ_sram.sh
@@ -13,14 +13,19 @@
 #
 # Dump and parse OCC logs:
 # - one time preparation:
-#	awk -F'\\|\\|' '{printf("%8.8x   %s\n", $1, $2)}' occ/obj/occStringFile > trexhex
+#	awk -F'\\|\\|' '{printf("%8.8x   %s:%%d: %s\n", $1, $3, $2)}' occStringFile | \
+#	sed "s/%p/%x/g" > trexhex
 #
 # - dumping
-#	./dump_occ_sram.sh <g_trac_{err,inf,imp}_buffer+0x28> 0x2000 | awk -f awk_program_occ -n
+#	./dump_occ_sram.sh <g_trac_{err,inf,imp}_buffer+0x28> 0x1fd8 | \
+#	sed "s/.\{27\}\(.\{8\}\)\(.\{8\}\).*/\1\n\2/" | \
+#	awk -f awk_program_occ -n
 #
 # No need for 'tac', OCC's format can be parsed in chronological order. It
 # prints timestamps before lines so you can 'cat' all 3 logs together before
 # passing it to 'awk' and 'sort' the output.
+# Some manual splitting may be required around the wrap-around, especially if
+# the entries are not 8B aligned...
 
 if [ $# -lt 1 -o $(($1)) -eq 0 ]; then
 	echo "Usage: $0 OCB_address [size]"
